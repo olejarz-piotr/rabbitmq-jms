773fdd97aa8c1b9af96394f0999099d787fbbbe7
OlejarzPiotr <olejarz-piotr@o2.pl>
2020-11-20 01:14:26 +0100

    Initial commit

---
+++ src/main/java/pl/olejarz/rabbitmqjms/RabbitmqJmsApplication.java
@@ -0,0 +1,13 @@
+package pl.olejarz.rabbitmqjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqJmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/olejarz/rabbitmqjms/RabbitmqJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.olejarz.rabbitmqjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
